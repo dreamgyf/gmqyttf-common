77e396db189bc9ea3927d56a477d78e7cb9d0851
Gao Yunfeng <g2409197994@gmail.com>
2020-08-23 00:08:36 +0800

    完成publish packet单元测试

---
+++ src/test/java/com/dreamgyf/gmqyttf/common/packet/MqttPublishPacketTest.java
@@ -0,0 +1,33 @@
+package com.dreamgyf.gmqyttf.common.packet;
+
+import com.dreamgyf.gmqyttf.common.enums.MqttVersion;
+import com.dreamgyf.gmqyttf.common.exception.MqttPacketParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MqttPublishPacketTest {
+
+    @Test
+    public void testBuildAndParse() throws MqttPacketParseException {
+        MqttPublishPacket packet = new MqttPublishPacket.Builder()
+                .DUP(false)
+                .QoS(0)
+                .RETAIN(false)
+                .id((short) 1000)
+                .topic("/topic/test/#")
+                .message("test message")
+                .build(MqttVersion.V3_1_1);
+
+        packet.parse(MqttVersion.V3_1_1);
+
+        Assert.assertEquals(packet.isDUP(), false);
+        Assert.assertEquals(packet.getQoS(), 0);
+        Assert.assertEquals(packet.isRETAIN(), false);
+        Assert.assertEquals(packet.getId(), 1000);
+        Assert.assertEquals(packet.getTopic(), "/topic/test/#");
+        Assert.assertEquals(packet.getMessage(), "test message");
+    }
+
+}
