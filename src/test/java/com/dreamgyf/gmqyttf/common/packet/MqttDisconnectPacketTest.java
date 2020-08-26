f58c6a70f5f9349e5f9deebf8c0fb46c8a393be4
Gaoyunfeng <yunfeng.gao@shanbay.com>
2020-08-26 13:42:41 +0800

    增加disconnect packet及其单元测试

---
+++ src/test/java/com/dreamgyf/gmqyttf/common/packet/MqttDisconnectPacketTest.java
@@ -0,0 +1,20 @@
+package com.dreamgyf.gmqyttf.common.packet;
+
+import com.dreamgyf.gmqyttf.common.enums.MqttVersion;
+import com.dreamgyf.gmqyttf.common.exception.MqttPacketParseException;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MqttDisconnectPacketTest {
+
+    @Test
+    public void testBuildAndParse() throws MqttPacketParseException {
+        MqttDisconnectPacket packet = new MqttDisconnectPacket.Builder()
+                .build(MqttVersion.V3_1_1);
+
+        byte[] answer = new byte[2];
+        answer[0] = (byte) 0b11100000;
+
+        Assert.assertArrayEquals(packet.getPacket(), answer);
+    }
+}
