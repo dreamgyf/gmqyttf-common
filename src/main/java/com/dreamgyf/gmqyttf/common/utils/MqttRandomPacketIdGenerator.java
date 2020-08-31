e44176fbe652edfa9254736994f858d47b6e3da7
Gaoyunfeng <yunfeng.gao@shanbay.com>
2020-08-31 16:42:40 +0800

    sync packet id generator

--- src/main/java/com/dreamgyf/gmqyttf/common/utils/MqttRandomPacketIdGenerator.java
+++ src/main/java/com/dreamgyf/gmqyttf/common/utils/MqttRandomPacketIdGenerator.java
@@ -7,7 +7,7 @@ import java.util.Set;
  * author: dreamgyf
  * email: g2409197994@gmail.com
  * 一个用于生成随机报文标识符的类
- * 暂不考虑异步操作
+ * 线程同步
  */
 public class MqttRandomPacketIdGenerator {
 
@@ -17,21 +17,25 @@ public class MqttRandomPacketIdGenerator {
     private final static Set<Short> idSet = new HashSet<>();
 
     public short next() {
-        short id;
-        do {
-            short random = NumUtils.randomShort(true);
-            id = (short) Math.max(1, random);
-        } while (idSet.contains(id));
-        idSet.add(id);
-        return id;
+        synchronized (this) {
+            short id;
+            do {
+                short random = NumUtils.randomShort(true);
+                id = (short) Math.max(1, random);
+            } while (idSet.contains(id));
+            idSet.add(id);
+            return id;
+        }
     }
 
     public boolean remove(short id) {
-        if (idSet.contains(id)) {
-            idSet.remove(id);
-            return true;
+        synchronized (this) {
+            if (idSet.contains(id)) {
+                idSet.remove(id);
+                return true;
+            }
+            return false;
         }
-        return false;
     }
 
     public static MqttRandomPacketIdGenerator create() {
